bb0c7c9a33545f5d8c869671957e2a64e98cf909
Kevin Sasse <sassek70@gmail.com>
2022-12-18 18:49:29 -0500

    finished lab

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,36 @@
-// add code snippets from README
+let state;
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+// function reducer(state = { count: 0 }, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+const dispatch = (action) => {
+    state = reducer(state, action);
+    render()
+}
+
+const render = () => {
+    const container = document.getElementById("container");
+    container.textContent = state.count;
+  }
+
+  dispatch({ type: "@@INIT" });
+  
+  const button = document.getElementById("button");
+
+  button.addEventListener("click", () => {
+    dispatch({ type: "counter/increment" });
+  });
+  
